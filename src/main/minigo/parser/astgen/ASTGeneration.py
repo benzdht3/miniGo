7801fb1fa0fdf70fc48f87276a554ca4004ca968
thiendh3 <thiendh@zigexn.vn>
2025-02-22 08:37:52 +0700

    init ass2

--- src/main/minigo/parser/astgen/ASTGeneration.py
+++ src/main/minigo/parser/astgen/ASTGeneration.py
@@ -1,14 +1,8 @@
-# Generated from main/minigo/parser/MiniGo.g4 by ANTLR 4.9.2
-from antlr4 import *
-if __name__ is not None and "." in __name__:
-    from .MiniGoParser import MiniGoParser
-else:
-    from MiniGoParser import MiniGoParser
+from MiniGoVisitor import MiniGoVisitor
+from MiniGoParser import MiniGoParser
+from AST import *
 
-# This class defines a complete generic visitor for a parse tree produced by MiniGoParser.
-
-class MiniGoVisitor(ParseTreeVisitor):
-
+class ASTGeneration(MiniGoVisitor):
     # Visit a parse tree produced by MiniGoParser#program.
     def visitProgram(self, ctx:MiniGoParser.ProgramContext):
         return self.visitChildren(ctx)
@@ -310,4 +304,3 @@ class MiniGoVisitor(ParseTreeVisitor):
 
 
 
-del MiniGoParser
